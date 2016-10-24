d161b560b6416380d3a28ae5c77dfabe913f9228
lcarranco <larry@carrancomarketing.com>
2016-10-24 16:46:31 -0500

    Working program that outputs the string in the variable str

--- exerciseA6.s
+++ exerciseA6.s
@@ -1,1 +1,14 @@
-# A program that computes the sum of an array of elements
+# A program that simulates an adding machine that will repeatedly read in integers and adds them into a running sum. The program will stop when it gets an input that is 0, printing out the sum at that point.
+
+        .data
+str:    .asciiz "This is a program that will compute the sum of the numbers the user has entered (To stop the program enter '0')"
+str1:   .asciiz "Enter a number (To exit enter '0'): "
+        .text
+        .globl main
+
+main:   li		$v0, 4		# System call code to print string   # $v0 = 4 
+        la		$a0, str	# 
+        syscall
+
+        li		$v0, 10		# Exits the program  # $v0 = 10
+        syscall
