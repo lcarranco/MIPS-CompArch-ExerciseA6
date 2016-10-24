37893ed319a4b2fcf07cac1ebc7521fd854d10d1
lcarranco <larry@carrancomarketing.com>
2016-10-24 16:50:29 -0500

    Prints a second string. Added spaced between the first and second strings

--- exerciseA6.s
+++ exerciseA6.s
@@ -1,13 +1,17 @@
 # A program that simulates an adding machine that will repeatedly read in integers and adds them into a running sum. The program will stop when it gets an input that is 0, printing out the sum at that point.
 
         .data
-str:    .asciiz "This is a program that will compute the sum of the numbers the user has entered (To stop the program enter '0')"
+str:    .asciiz "This is a program that will compute the sum of the numbers the user has entered (To stop the program enter '0')\n\n"
 str1:   .asciiz "Enter a number (To exit enter '0'): "
         .text
         .globl main
 
 main:   li		$v0, 4		# System call code to print string   # $v0 = 4 
-        la		$a0, str	# 
+        la		$a0, str	# Print str
+        syscall
+
+        li		$v0, 4		# System call code to print string   # $v0 = 4 
+        la		$a0, str1	# Print str1
         syscall
 
         li		$v0, 10		# Exits the program  # $v0 = 10
