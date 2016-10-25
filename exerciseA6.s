625cb99ab472c869f3f5ee158b3aaa77a93c96de
lcarranco <larry@carrancomarketing.com>
2016-10-25 09:48:05 -0500

    It is now able to print the sum of 2 numbers. Need to fix trying to add negatives and trying to add more than 2 numbers.

--- exerciseA6.s
+++ exerciseA6.s
@@ -1,7 +1,7 @@
 # A program that simulates an adding machine that will repeatedly read in integers and adds them into a running sum. The program will stop when it gets an input that is 0, printing out the sum at that point.
 
         .data
-str:    .asciiz "This is a program that will compute the sum of the numbers the user has entered (To stop the program enter '0')\n\n"
+str:    .asciiz "This is a program that will compute the sum of the numbers the user has entered\n\n"
 str1:   .asciiz "Enter a number (To exit enter '0'): "
         .text
         .globl main
@@ -15,7 +15,15 @@ loop:   li		$v0, 4		# System call code to print string   # $v0 = 4
         syscall
         li		$v0, 5		# System call code to read integer  # $v0 = 5 
         syscall
+        beq		$v0, $zero, exit	# if $v0 == $zero then exit
+        move 	        $s0, $v0		# $s0 = $v0
+        add		$s0, $s0, $s0		# $s0 = $s0 + $s0 
+        j		loop				# jump to loop
         
+               
 
-exit:   li		$v0, 10		# Exits the program  # $v0 = 10
+exit:   li		$v0, 1		# $v0 = 1 
+        move 	$a0, $s0		# $a0 = 
+        syscall
+        li		$v0, 10		# Exits the program  # $v0 = 10
         syscall
