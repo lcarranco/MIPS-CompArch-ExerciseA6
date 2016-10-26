b3f8bfc291f79591c138ef1562d5228cc013ae3c
lcarranco <larry@carrancomarketing.com>
2016-10-26 10:27:45 -0500

    Completed program

--- exerciseA6.s
+++ exerciseA6.s
@@ -6,24 +6,23 @@ str1:   .asciiz "Enter a number (To exit enter '0'): "
         .text
         .globl main
 
-main:   li		$v0, 4		# System call code to print string   # $v0 = 4 
-        la		$a0, str	# Print str
+main:   li		$v0, 4		        # System call code to print string 
+        la		$a0, str	        # Print str
         syscall
 
-loop:   li		$v0, 4		# System call code to print string   # $v0 = 4 
-        la		$a0, str1	# Print str1
+loop:   li		$v0, 4		        # System call code to print string 
+        la		$a0, str1	        # Print str1
         syscall
-        li		$v0, 5		# System call code to read integer  # $v0 = 5 
+        li		$v0, 5		        # System call code to read integer  # $v0 = 5 
         syscall
-        beq		$v0, $zero, exit	# if $v0 == $zero then exit
-        move 	        $s0, $v0		# $s0 = $v0
-        add		$s0, $s0, $s0		# $s0 = $s0 + $s0 
-        j		loop				# jump to loop
-        
-               
+        beq		$v0, $zero, exit	# if $v0 == zero then exit 
+        add		$s6, $s3, $s6		# add i to array and store in array
+        move 	        $s2, $v0		# move integer to $s2
+        add		$s6, $s2, $s6		# add $s2 to array
+        j		loop			# jump to loop                                          
 
-exit:   li		$v0, 1		# $v0 = 1 
-        move 	$a0, $s0		# $a0 = 
+exit:   li		$v0, 1		        # System call to print integer 
+        move 	        $a0, $s6		
         syscall
         li		$v0, 10		# Exits the program  # $v0 = 10
         syscall
